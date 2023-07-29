e5d85c2c0b42af1e9e12a3f09312ffc97bce2f4f
Doan Hoa (段梨花) <q81188118@gmail.com>
2023-07-29 23:49:54 +0700

    Update Budget.js
    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,41 +2,48 @@ import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
- const { budget, remaining } = useContext(AppContext);
- const [editableBudget, setEditableBudget] = useState(budget);
+    const { budget, expenses, currency } = useContext(AppContext);
+    const [ editableBudget, setEditableBudget] = useState(budget);
 
- useEffect(() => {
- setEditableBudget(budget);
-  }, [budget]);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
 
- const validateBudget = (value) => {
- if(value > 20000) {
- alert("Budget could not exceed 20000");
- return false;
-    }
- if(value < remaining) {
- alert(`Budget should not be lower than remaining of ${remaining}`);
- return false;
-    }
- return true;
-  };
+    console.log({totalExpenses})
+
+    useEffect(() => {
+    setEditableBudget(budget);
+    }, [budget]);
+
+    const validateBudget = (value) => {
+        if(value > 20000) {
+        alert("Budget could not exceed 20000");
+        return false;
+        }
+        if(value <= totalExpenses ) {
+        alert(`You cannot reduce the budget value lower than spending!`);
+        return false;
+        }
+        return true;
+    };
  
  const handleInputChange = (e) => {
- let value = parseInt(e.target.value, 10);
- setEditableBudget(value);
+    let value = parseInt(e.target.value, 10);
+    setEditableBudget(value);
   };
 
-  const handleInputBlur = (e) => {
-  let value = parseInt(e.target.value, 10);
-  if (!validateBudget(value)) {
-    setEditableBudget(budget);
-  }
-};
+    const handleInputBlur = (e) => {
+        let value = parseInt(e.target.value, 10);
+        if (!validateBudget(value)) {
+            setEditableBudget(budget);
+        }
+    };
 
  return (
  <div className='alert alert-secondary'>
- <span>Budget: </span>
-    <input type="number" value={editableBudget} min={totalExpenses} max={20000} step={10} onChange={handleInputChange} onBlur={handleInputBlur} />
+    <span>Budget: </span>
+    <span>{ currency }</span>
+    <input type="number" value={editableBudget} min={ totalExpenses } max={20000} step={10} onChange={handleInputChange} onBlur={handleInputBlur} />
  </div>
   );
 };
